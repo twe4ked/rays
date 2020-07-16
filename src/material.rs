4c7d0c95bde3601cd2cb5a39616acf0482b61722
Odin Dutton <odindutton@gmail.com>
2020-07-16 18:57:03 +1000

    Remove negated conditional

--- src/material.rs
+++ src/material.rs
@@ -64,7 +64,7 @@ impl Material for Metal {
 fn random_in_unit_sphere() -> Vec3 {
     loop {
         let p = Vec3::random(-1.0, 1.0);
-        if !(p.length_squared() >= 1.0) {
+        if p.length_squared() < 1.0 {
             return p;
         }
     }
